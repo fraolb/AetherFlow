117c37523756fae3ae390461427fe9994153683d
fraolb <Fraolbereket@gmail.com>
2025-10-17 22:03:51 +0300

    add wagmi provider

---
+++ frontend/app/Provider.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import "@rainbow-me/rainbowkit/styles.css";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  RainbowKitProvider,
+  connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
+import { WagmiProvider, createConfig, http } from "wagmi";
+import { base } from "wagmi/chains";
+
+import { injectedWallet } from "@rainbow-me/rainbowkit/wallets";
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Recommended",
+      wallets: [injectedWallet],
+    },
+  ],
+  {
+    appName: "AetherFlow",
+    projectId: process.env.WC_PROJECT_ID ?? "044601f65212332475a09bc14ceb3c34",
+  }
+);
+
+export const config = createConfig({
+  connectors,
+  chains: [base],
+  transports: {
+    [base.id]: http(),
+  },
+});
+
+const queryClient = new QueryClient();
+
+export function Provider({ children }: { children: React.ReactNode }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+}
